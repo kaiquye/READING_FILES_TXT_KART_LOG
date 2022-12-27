f340f05e9a4248f9c5ed66945e8ed4ba623be8dd
kaiquye <kaiquemendesilva82@gmail.com>
2022-12-27 10:57:52 -0300

    first commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 import applicationRoutesRoutes from './shared/routes/applicationRoutes.routes';
 const log = console.log;
@@ -7,6 +8,13 @@ const application = () => {
   dotenv.config();
 
   const app = express();
+  app.use(
+    cors({
+      origin: ['http://localhost:3000', 'http://127.0.0.1', 'http://104.142.122.231'],
+      credentials: true,
+      exposedHeaders: ['set-cookie'],
+    }),
+  );
   app.use(express.json());
   app.use(applicationRoutesRoutes);
   app.listen(8080, () => log('start server...'));
